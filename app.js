300daf5dc7c12f49016eb005ef5b3ef360ba21b2
Kzrvc <anto.kozarevic@gmail.com>
2025-01-09 19:59:02 +0100

    second change

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-const movieLists = document.querySelectorAll(".movie-list");
-
-const ball = document.querySelector(".toggle-ball");
-const items = document.querySelectorAll(
-  ".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle"
-);
-
-ball.addEventListener("click", () => {
-  items.forEach((item) => {
-    item.classList.toggle("active");
-  });
-  ball.classList.toggle("active");
-});
-
-
 const buttons = document.querySelectorAll(".movie-list-item-button");
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
